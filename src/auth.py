f883045faea10f0c4e65cb6715ae2e33e0c1d770
hartlucas <hart.lucas02@gmail.com>
2024-01-03 15:51:46 -0700

    initialized template

---
+++ src/auth.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+def csmsso_auth(driver):
+    MINES_SSO_USERNAME = input("Enter your Mines SSO username: ")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    MINES_SSO_PASSWORD = getpass(prompt="Enter your Mines SSO password: ")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Write username and password to the .env file
+    with open("../.env", 'a') as env:
+        env.write(f"MINES_SSO_USERNAME='{username}'\n")
+        env.write(f"MINES_SSO_PASSWORD='{password}'\n")
+
+    try:
+        driver.get('https://my.mines.edu/')
+        wait = WebDriverWait(driver, 20, poll_frequency=0.5)
+        wait.until(EC.presence_of_element_located((By.ID, 'input28'))).send_keys(username)
+        driver.find_element(by=By.CLASS_NAME, value="o-form-button-bar").click()
+
+        wait.until(EC.presence_of_element_located((By.ID, 'input59'))).send_keys(password)
+        driver.find_element(by=By.CLASS_NAME, value='o-form-button-bar').click()
+
+        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'o-form-button-bar'))).click()
+
+        # Pause and wait for user confirmation for MFA
+        input("Please confirm Okta MFA on your phone, then press Enter to continue.")
+      
+        wait.until(EC.presence_of_element_located((By.ID, 'trust-browser-button'))).click()
+
+        print(f'Successfully authenticated at {driver.current_url}')
+        return 0
+    except TimeoutException:
+        print('Webdriver timed out during authentication')
+        return 1
+    except Exception as e:
+        print(f'Failed to authenticate: {e}')
+        return 1
