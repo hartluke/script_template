d83a6543a00513aaa628be327bfe775ba04d30b9
Luke Hart (Student) <hart.lucas02@gmail.com>
2024-02-12 14:05:44 -0700

    script templates cleanup

--- src/prod/script.py
+++ src/prod/script.py
@@ -1,18 +1,12 @@
 # luke hart @ colorado school of mines
 # script template
-from pathlib import Path
-from tqdm import tqdm
 from dotenv import load_dotenv
-from datetime import datetime
-import time
 
 def func():
     load_dotenv()
 
     try:
         # TODO: implement script
-        for i in range(100):
-            time.sleep(0.2)
         return 0
     except Exception as e:
         print(f'Script failed: {e}')
--- src/sb/script.py
+++ src/sb/script.py
@@ -1,21 +1,9 @@
 # luke hart @ colorado school of mines
 # script template
-from pathlib import Path
-from tqdm import tqdm
 from dotenv import load_dotenv
 
 def func():
-    os.system('cls' if os.name == 'nt' else 'clear')
     load_dotenv()
-    dist = Path("../dist")
-    logs = Path("../logs")
-    logs.mkdir(exist_ok=True)
-    
-    logging.basicConfig(
-        filename=logs / f"{datetime.now().isoformat().replace(':', '_')}.log",
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
-    )
 
     try:
         # TODO: implement script
@@ -23,3 +11,6 @@ def func():
     except Exception as e:
         print(f'Script failed: {e}')
         return 1
+    
+if __name__ == "__main__":
+    func()
